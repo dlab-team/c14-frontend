1aff81e04726fc9c0c7ba0d1952fc03d8dfa7ca6
Felipe Burboa <felipeburboa1991@gmail.com>
2023-12-05 20:16:19 -0300

    feat: adding login view and lines component

--- src/components/Lines.jsx
+++ src/components/Lines.jsx
@@ -9,7 +9,7 @@ const Lines = () => {
         <div className="flex-1 flex items-center justify-center bg-gradient-to-b from-purple-600 via-purple-500 to-purple-500">
         <img src="/logo/logo-3xi-blanco.png" alt="Logo 3xi" className="h-14" />
         </div>
-        <div className="flex-1 flex items-center justify-center bg-gradient-to-b from-red-600 via-rose-500 to-pink-700">
+        <div className="flex-1 flex bg-gradient-to-b from-red-600 via-rose-500 to-pink-700">
         </div>
         <div className="flex-1 flex items-center justify-center bg-gradient-to-b from-orange-400 via-orange-400 to-orange-500">
         <img src="/logo/logo-3xi-blanco.png" alt="Logo 3xi" className="h-14" />
