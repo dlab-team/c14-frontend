e2fd36da984e486f0cb76c49a5e04101826f058c
José Campos <josecamposh95@gmail.com>
2023-12-20 18:53:47 -0300

    move admin header from layout to page

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -2,13 +2,7 @@ import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 
 import Sidebar from '@/components/Sidebar';
-import AdminHeader from '@/components/admin/AdminHeader';
 import useAuthStore from '../store/useAuthStore';
-
-const headerInfo = {
-  title: 'Bienvenido, Usuario!',
-  description: 'Información general sobre los resultados de los encuestados.'
-}
 
 export const AdminLayout = () => {
   const navigate = useNavigate();
@@ -22,8 +16,7 @@ export const AdminLayout = () => {
   return (
     <div className="min-h-screen w-full grid grid-cols-1 xl:grid-cols-10">
       <Sidebar />
-      <AdminHeader title={headerInfo.title} description={headerInfo.description} />
-      <div className="xl:col-span-8 h-screen p-8 relative -z-10">
+      <div className="xl:col-span-8 h-screen relative -z-10">
         <Outlet />
       </div>
     </div>
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,24 +1,14 @@
-// Admin.jsx
-import useLogout from '../../hooks/useLogout';
-import Button from '../../layouts/Button';
-import { toast } from 'sonner';
+import AdminHeader from '@/components/admin/AdminHeader';
 
 const Admin = () => {
-  const title = 'Logout';
-  const { logout } = useLogout();
-
-  const handleLogoutClick = async () => {
-    const isConfirmed = window.confirm('¿Estás seguro de que deseas salir?');
-    if (isConfirmed) {
-      await logout();
-      toast.success('Sesión cerrada exitosamente');
-    }
-  };
-
   return (
-    <div onClick={handleLogoutClick}>
-      <Button title={title}></Button>
-    </div>
+    <>
+      <AdminHeader
+        title={`Bienvenido, Usuario!`}
+        description="Información general sobre los resultados de los encuestados."
+      />
+      <main className="p-8"></main>
+    </>
   );
 };
 
