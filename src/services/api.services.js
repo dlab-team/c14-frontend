884b7ed7d7fd36fcc07ecb5ab9af12d6d126d7f7
José Campos <josecamposh95@gmail.com>
2023-12-20 21:12:05 -0300

    add withCredentials to axios instance

--- src/services/api.services.js
+++ src/services/api.services.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const apiUrl = import.meta.env.API_URL;
 
 const apiService = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: apiUrl || 'http://localhost:3001/api',
+  withCredentials: true,
 });
 
 export default apiService;
