e5efde20072891fc5cf2c0ffda614a30eed14ddc
Jonathan Riffo <jonathan.riffo7@gmail.com>
2024-01-02 13:36:32 -0300

    fix: adding movil responsiveness

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,12 +1,8 @@
 import AdminHeader from '@/components/admin/AdminHeader';
 import useAuthStore from '@/store/useAuthStore';
-import AdminModal from '@/components/admin/AdminModal';
-import Button from '@/layouts/Button';
-import { useState } from 'react';
 
 const Admin = () => {
   const { user } = useAuthStore();
-  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
@@ -14,11 +10,7 @@ const Admin = () => {
         title={`Bienvenido, ${user?.firstName}`}
         description="Información general sobre los resultados de los encuestados."
       />
-      <AdminModal />
-      <main className="p-8">
-        <Button title={'Abrir modal'} />
-      </main>
-      {showModal && <AdminModal setShowModal={setShowModal} />}
+      <main className="p-8"></main>
     </>
   );
 };
