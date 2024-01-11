86221c2303eda76c064dfa2e06374ab872aeb7ec
José Campos <josecamposh95@gmail.com>
2024-01-10 21:02:48 -0300

    save opposite political phrases after first load

--- src/pages/OppositeQuestions/OppositeQuestions.jsx
+++ src/pages/OppositeQuestions/OppositeQuestions.jsx
@@ -1,22 +1,31 @@
-import useGetOppositePoliticalPhrases from '@/hooks/useGetOppositePoliticalPhrases'
-import useFormStore from '@/store/useFormStore';
-import  Button from '../../layouts/Button'
-import CardOpposite from "./Components/CardOpposite"
-import HeaderOpposite from "./Components/HeaderOpposite"
+import { useEffect } from 'react';
 
+import useFormStore from '@/store/useFormStore';
+import useGetOppositePoliticalPhrases from '@/hooks/useGetOppositePoliticalPhrases';
+import Button from '../../layouts/Button';
+import CardOpposite from './Components/CardOpposite';
+import HeaderOpposite from './Components/HeaderOpposite';
 
 const OppositeQuestions = ({ handleStep }) => {
   const howCompare = useFormStore.getState().howCompare;
-  const phrases = useGetOppositePoliticalPhrases(howCompare);
-const handleOnClick = () => {
-  handleStep();
-};
+  const opossitePoliticalResult = useFormStore(s => s.opossitePoliticalResult);
+  const setOpossitePoliticalResult = useFormStore(s => s.setOpossitePoliticalResult);
+  const { data: phrases } = useGetOppositePoliticalPhrases(howCompare || 'Extremo 1');
+  const handleOnClick = () => {
+    handleStep();
+  };
+
+  useEffect(() => {
+    if (!opossitePoliticalResult?.length) {
+      setOpossitePoliticalResult(phrases);
+    }
+  }, [phrases, opossitePoliticalResult, setOpossitePoliticalResult]);
 
   return (
     <>
-      <HeaderOpposite></HeaderOpposite>
-      {phrases.data.map(phrase => {
-        return <CardOpposite phrase={phrase} key={phrase.id}></CardOpposite>;
+      <HeaderOpposite />
+      {opossitePoliticalResult?.map(phrase => {
+        return <CardOpposite phrase={phrase} key={phrase.id} />;
       })}
 
       <div className=" flex justify-end mx-auto mb-40 max-w-2xl">
@@ -25,4 +34,4 @@ const handleOnClick = () => {
     </>
   );
 };
-export default OppositeQuestions
+export default OppositeQuestions;
