61e66bba67fef7d4bdba0633ab8a5d80ee035c36
José Campos <josecamposh95@gmail.com>
2024-01-10 21:02:22 -0300

    add value & handleChange props to CustomSlider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,39 +1,16 @@
+import PropTypes from 'prop-types';
 import Slider from '@mui/material/Slider';
 import './Slider.css';
-import { useState } from 'react';
-import useFormStore from '@/store/useFormStore';
 
-function valuetext(value) {
-    return `${value}%`;
-  }
+function valueText(value) {
+  return `${value}%`;
+}
 
-export default function CustomSlider({color,phrase}){
-  const setOpossitePoliticalResult = useFormStore(state => state.setOpossitePoliticalResult);
-  const opossitePoliticalResult = useFormStore(state => state.opossitePoliticalResult);
-  const [value, setValue] = useState(50);
-  
-  function handleChange(e, newValue) {
-    const index = opossitePoliticalResult.findIndex(item => item.id === phrase.id);
-
-    if (index !== -1) {
-      const updatedResults = [...opossitePoliticalResult];
-      updatedResults[index] = { percentage: newValue, id: phrase.id, text: phrase.text };
-      setOpossitePoliticalResult(updatedResults);
-    } else {
-      setOpossitePoliticalResult([
-        ...opossitePoliticalResult,
-        { percentage: newValue, id: phrase.id, text: phrase.text },
-      ]);
-    }
-
-    setValue(newValue);
-  }
-
-
+export default function CustomSlider({ color = '#66B947', value = 50, handleChange }) {
   return (
     <Slider
       value={value}
-      valueLabelFormat={valuetext}
+      valueLabelFormat={valueText}
       step={1}
       valueLabelDisplay="on"
       sx={{
@@ -44,3 +21,8 @@ export default function CustomSlider({color,phrase}){
   );
 }
 
+CustomSlider.propTypes = {
+  color: PropTypes.string,
+  value: PropTypes.number,
+  handleChange: PropTypes.func,
+};
