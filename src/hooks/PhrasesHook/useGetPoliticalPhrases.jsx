873fc4a0744c2222cbec67921232a811249ff293
José Campos <josecamposh95@gmail.com>
2024-01-17 18:19:15 -0300

    fix use get political phrases

--- src/hooks/PhrasesHook/useGetPoliticalPhrases.jsx
+++ src/hooks/PhrasesHook/useGetPoliticalPhrases.jsx
@@ -2,16 +2,14 @@ import { PhrasesService } from '@/services/phrases.service';
 import { toast } from 'sonner';
 import { useQuery } from 'react-query';
 
-const useGetPoliticalPhrases = valor => {
-  const { data, isLoading, isError, refetch } = useQuery(
-    'politicalPhrases',
-    PhrasesService.getPoliticalPhrases(valor),
-    {
-      onError: error => {
-        toast.error(error?.message || 'Error al obtener las opciones');
-      },
-    }
-  );
+const useGetPoliticalPhrases = optionId => {
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryHash: 'politicalPhrases',
+    queryFn: () => PhrasesService.getPoliticalPhrases(optionId),
+    onError: error => {
+      toast.error(error?.message || 'Error al obtener las opciones');
+    },
+  });
 
   return { data, isLoading, isError, refetch };
 };
