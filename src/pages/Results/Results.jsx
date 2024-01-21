a6d5480812ff1abb37902dc3fcdcd17a4e9d03fb
Arturo Muñoz <boomer_rap@hotmail.com>
2024-01-21 15:19:22 -0500

    feat:Vista resultados Mi percepción

--- src/pages/Results/Results.jsx
+++ src/pages/Results/Results.jsx
@@ -1,6 +1,7 @@
 import AdminHeader from '@/components/admin/AdminHeader';
 import { ResultsFooter } from './components/ResultsFooter';
 import Comparison from './components/Comparison.jsx';
+import { Perception } from './components/Perception';
 
 const Results = () => {
   return (
@@ -12,6 +13,7 @@ const Results = () => {
         positionTitle="items-center"
       />
       <Comparison />
+      <Perception />
       <ResultsFooter />
     </div>
   );
--- src/pages/Results/components/testComparison.js
+++ src/pages/Results/components/testComparison.js
@@ -1,38 +1,44 @@
 export const testComparison = [
-    {
-      item: 1,
-      phrase: 'Frase1 Frase1 Frase1 Frase1 Frase1 Frase1 Frase1 Frase1 Frase1 Frase1',
-      value: 77,
-      answer: 'En desacuerdo'
-    },
-    {
-      item: 2,
-      phrase: 'Frase2 Frase2 Frase2 Frase2 Frase2 Frase2 Frase2 Frase2 Frase2 Frase2',
-      value: 80,
-      answer: 'De acuerdo'
-    },
-    {
-      item: 3,
-      phrase: 'Frase3 Frase3 Frase3 Frase3 Frase3 Frase3 Frase3 Frase3 Frase3 Frase3',
-      value: 20,
-      answer: 'En desacuerdo'
-    },
-    {
-      item: 4,
-      phrase: 'Frase4 Frase4 Frase4 Frase4 Frase4 Frase4 Frase4 Frase4 Frase4 Frase4',
-      value: 10,
-      answer: 'De acuerdo'
-    },
-    {
-      item: 5,
-      phrase: 'Frase5 Frase5 Frase5 Frase5 Frase5 Frase5 Frase5 Frase5 Frase5 Frase5',
-      value: 45,
-      answer: 'En desacuerdo'
-    },
-    {
-      item: 6,
-      phrase: 'Frase6 Frase6 Frase6 Frase6 Frase6 Frase6 Frase6 Frase6 Frase6 Frase6',
-      value: 100,
-      answer: 'De acuerdo'
-    }
-  ];
+  {
+    item: 1,
+    phrase: 'Frase1 Frase1 Frase1 Frase1 Frase1 Frase1 Frase1 Frase1 Frase1 Frase1',
+    value: 77,
+    answer: 'En desacuerdo',
+    investigation: 90,
+  },
+  {
+    item: 2,
+    phrase: 'Frase2 Frase2 Frase2 Frase2 Frase2 Frase2 Frase2 Frase2 Frase2 Frase2',
+    value: 80,
+    answer: 'De acuerdo',
+    investigation: 95,
+  },
+  {
+    item: 3,
+    phrase: 'Frase3 Frase3 Frase3 Frase3 Frase3 Frase3 Frase3 Frase3 Frase3 Frase3',
+    value: 20,
+    answer: 'En desacuerdo',
+    investigation: 50,
+  },
+  {
+    item: 4,
+    phrase: 'Frase4 Frase4 Frase4 Frase4 Frase4 Frase4 Frase4 Frase4 Frase4 Frase4',
+    value: 10,
+    answer: 'De acuerdo',
+    investigation: 60,
+  },
+  {
+    item: 5,
+    phrase: 'Frase5 Frase5 Frase5 Frase5 Frase5 Frase5 Frase5 Frase5 Frase5 Frase5',
+    value: 45,
+    answer: 'En desacuerdo',
+    investigation: 10,
+  },
+  {
+    item: 6,
+    phrase: 'Frase6 Frase6 Frase6 Frase6 Frase6 Frase6 Frase6 Frase6 Frase6 Frase6',
+    value: 99,
+    answer: 'De acuerdo',
+    investigation: 10,
+  },
+];
