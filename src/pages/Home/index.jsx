e0cdd20e4764eee1c67113faad36176acd4908a6
diegorojas89z <rojasdiego.89@gmail.com>
2024-01-22 22:03:03 -0300

    fix: changed logo in home page & some text

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -43,37 +43,13 @@ export default function Home() {
       <Toaster position="top-center" />
       <Terms isOpen={isModalOpen} onClose={closeModal} onAccept={handleAccept} />
       <div className="grid lg:grid-cols-2 h-[90vh] ">
-        <div className="hidden lg:flex flex-col h-full">
-          <img src="/logo/criteria/criteriaLogo.png" alt="Logo criteria" className="w-72 p-12 " />
-          <div className="relative flex gap-2 items-end h-full">
-            <div className="bg-gradient-to-b from-teal-400 via-teal-500 to-teal-600 w-16 h-full shadow-2xl rounded-t-sm"></div>
-
-            <div className="bg-gradient-to-b from-lime-400 via-lime-500 to-lime-600 w-16 h-[96%] shadow-2xl rounded-t-sm pt-5"></div>
-
-            <div className="bg-gradient-to-b from-purple-500 via-purple-600 to-purple-700 w-16 h-[92%] shadow-2xl rounded-t-sm"></div>
-
-            <div className="bg-gradient-to-b from-red-500 via-pink-600 to-pink-700 w-16 h-[88%] shadow-2xl rounded-t-sm"></div>
-
-            <div className="bg-gradient-to-b from-orange-400 via-orange-500 to-orange-600 w-16 h-[84%] shadow-2xl rounded-t-sm"></div>
-
-            <div className="absolute top-28 left-64">
-              <img src="/logo/logo-3xi-negro.png" alt="Logo criteria" className="w-44 relative" />
-              <div className="flex flex-col leading-5 absolute -right-10 mt-3 font-medium">
-                <span>Inspirarnos</span>
-                <span>Incluirnos</span>
-                <span>Innovarnos</span>
-              </div>
-              <p className="text-start ">
-                <br />
-                <br />
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col items-center lg:justify-center lg:me-36 pt-20 lg:pt-0">
+        <img src="logo/logo-estudio.png" alt="Logo estudio" width={'60%'} className="mx-auto my-10 lg:my-10 lg:ml-20"/>
+        <div className="flex flex-col items-center lg:justify-center lg:me-36 lg:pt-0">
           <h1 className="text-4xl lg:text-5xl font-bold text-center mx-4">
-            Estudio Polarizaciones
+            Test de polarizaciones
           </h1>
+          <h2 className="text-2xl text-center mx-4"> Descúbrete y despolarízate!
+</h2>
           <p className="xl:text-xl text-center px-10 sm:px-28 lg:px-0 py-10 text-slate-600 w-[90%]">
             Esta encuesta es muy especial porque te vamos a desafiar. A continuación, te
             presentaremos una serie de frases que quizás no representen exactamente tu pensamiento e
@@ -82,39 +58,7 @@ export default function Home() {
             siempre se generan problemas, pero es la única manera que tenemos de medir opiniones en
             una encuesta.
           </p>
-          <div className="lg:hidden flex flex-col items-center h-[500px] pb-16 mx-6">
-            <div className="flex ">
-              <img
-                src="/logo/criteria/criteriaLogo.png"
-                alt="Logo criteria"
-                className="w-72 py-12"
-              />
-            </div>
-            <div className="relative flex gap-1 items-end h-full me-20">
-              <div className="bg-gradient-to-b from-teal-400 via-teal-500 to-teal-600 w-12 h-full shadow-2xl rounded-t-sm"></div>
-
-              <div className="bg-gradient-to-b from-lime-400 via-lime-500 to-lime-600 w-12 h-[95%] shadow-2xl rounded-t-sm pt-5"></div>
-
-              <div className="bg-gradient-to-b from-purple-500 via-purple-600 to-purple-700 w-12 h-[90%] shadow-2xl rounded-t-sm"></div>
-
-              <div className="bg-gradient-to-b from-red-500 via-pink-600 to-pink-700 w-12 h-[85%] shadow-2xl rounded-t-sm"></div>
-
-              <div className="bg-gradient-to-b from-orange-400 via-orange-500 to-orange-600 w-12 h-[80%] shadow-2xl rounded-t-sm"></div>
-
-              <div className="absolute top-24 -right-11">
-                <img src="/logo/logo-3xi-negro.png" alt="Logo criteria" className="w-24" />
-                <div className="flex flex-col leading-5 absolute -right-12 mt-1 font-medium">
-                  <span>Inspirarnos</span>
-                  <span>Incluirnos</span>
-                  <span>Innovarnos</span>
-                </div>
-                <p className="text-start ">
-                  <br />
-                  <br />
-                </p>
-              </div>
-            </div>
-          </div>
+          <div className="lg:hidden flex flex-col items-center mx-6"></div>
           <div className="lg:w-96">
             <p className="flex items-center justify-center mb-9">
               <input
@@ -125,7 +69,7 @@ export default function Home() {
               />
               <span className="font-medium text-sm">Estoy de acuerdo con los </span>
               <span className="underline cursor-pointer font-bold text-sm ml-1" onClick={openModal}>
-                terminos y condiciones
+                términos y condiciones
               </span>
             </p>
             <div className="lg:pb-0 pb-10">
