00e8426df9a10eaad65df55ad7d18c693df61329
Felipe Burboa <felipeburboa1991@gmail.com>
2024-01-25 12:48:36 -0300

    feat: adding persist middleware to zustand

--- src/store/useFormStore.js
+++ src/store/useFormStore.js
@@ -1,8 +1,8 @@
 import { create } from 'zustand';
-// import { persist } from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 
 const useFormStore = create(
-  // persist(
+  persist(
   set => ({
     politicalCharacterization: null,
     currentSurveySection: 0,
@@ -50,7 +50,7 @@ const useFormStore = create(
   {
     name: 'form-storage',
   }
-  // )
+  )
 );
 
 export default useFormStore;
