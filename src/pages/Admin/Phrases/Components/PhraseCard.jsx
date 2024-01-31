4429214105edb805566198a7781962a75b7f3390
Michelle Faure <faure.s.michelle@gmail.com>
2024-01-31 17:59:02 -0300

    filtro extremo de frase

--- src/pages/Admin/Phrases/Components/PhraseCard.jsx
+++ src/pages/Admin/Phrases/Components/PhraseCard.jsx
@@ -1,14 +1,26 @@
 import { FaRegTrashCan, FaRegEye, FaRegEyeSlash } from 'react-icons/fa6';
 import { FiEdit3 } from 'react-icons/fi';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { phrasesSchema } from '../../../../schemas/phrasesSchema';
 import { MdPercent } from 'react-icons/md';
+import useGetOptions from '@/hooks/OptionsHook/useGetOptions';
+import Select from 'react-select';
 
-const PhraseCard = ({ phrase }) => {
+const PhraseCard = ({ phrase, index }) => {
   const [showEdit, setShowEdit] = useState(true);
   const [activePhrase, setActivePhrase] = useState(true);
+  const { data: options, isLoading, isError } = useGetOptions();
+  const [filteredOptions, setsfilteredOptions] = useState([]);
+
+  useEffect(() => {
+    if (options) {
+      const filteredOptions = options.filter(item => item.polynomialId === phrase.polynomialId);
+      setsfilteredOptions(filteredOptions);
+    }
+  }, [options, phrase.polynomialId]);
+
   const {
     register,
     handleSubmit,
@@ -21,15 +33,15 @@ const PhraseCard = ({ phrase }) => {
     resolver: yupResolver(phrasesSchema),
   });
 
-  
   const onSubmit = data => {
-    console.log(data);
+    console.log(phrase);
   };
+
   return (
     <div className="border rounded border-slate-200 bg-white shadow-xl p-4 md:p-10 mb-10">
       <div className="flex justify-between items-center text-2xl">
         <div className="flex flex-wrap">
-          <p className="font-semibold me-2">Frase N°x</p>
+          <p className="font-semibold me-2">Frase N°{index+1}</p>
           <span className="text-xs text-[#34ABC9] font-medium bg-[#E8FAFF] py-2 px-3 rounded-full">
             {phrase.group}
           </span>
@@ -63,6 +75,9 @@ const PhraseCard = ({ phrase }) => {
       ) : (
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="relative my-10">
+            <span className="text-xs text-red-500">{errors?.phrase?.message}</span>
+          </div>
+          <div className="relative my-10">
             <input
               type="phrase"
               placeholder={'Escriba su pregunta aquí...'}
@@ -72,36 +87,32 @@ const PhraseCard = ({ phrase }) => {
             <span className="text-xs text-red-500">{errors?.phrase?.message}</span>
           </div>
 
-          <div className="flex justify-center md:justify-around flex-wrap text-xl mb-10 gap-8">
-            <select
-              className="border rounded-lg border-slate-200 px-3 font-semibold h-12 w-72 "
-              {...register('binomio')}
-            >
-              <option value="" defaultValue>
-                Identidad Politica
-              </option>
-              <option value="Opcion1">Opcion1</option>
-              <option value="Opcion2">Opcion2</option>
-              <option value="Opcion3">Opcion3</option>
-            </select>
-
-            <div className="flex items-stretch relative rounded-lg border border-slate-200 focus-within:outline-slate-500 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2">
-              <span className="absolute bottom-16 bg-white text-xs text-red-500">
-                {errors?.porcentaje?.message}
-              </span>
-              <input
-                type="number"
-                min="0"
-                max="100"
-                {...register('porcentaje')}
-                placeholder="acuerdo"
-                className="peer px-3 w-32 rounded-l-lg outline-none "
-              />
-              <div className="bg-[#34ABC9] text-white flex items-center rounded-r-lg px-3">
-                <MdPercent />
-              </div>
-            </div>
+          <div className="flex flex-col mx-auto  flex-wrap max-w-xl text-xl mb-10 gap-8">
+            {filteredOptions.map(poliOpt => {
+              return (
+                <div className="flex justify-between">
+                  <div className="rounded-lg border border-slate-200 w-1/2 p-2">{poliOpt.name}</div>
+                  <div className="flex items-stretch relative rounded-lg border border-slate-200 focus-within:outline-slate-500 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 max-h-11">
+                    <span className="absolute bottom-16 bg-white text-xs text-red-500">
+                      {errors?.porcentaje?.message}
+                    </span>
+                    <input
+                      type="number"
+                      min="0"
+                      max="100"
+                      {...register('porcentaje')}
+                      placeholder="acuerdo"
+                      className="peer px-3 w-32 rounded-l-lg outline-none "
+                    />
+                    <div className="bg-[#34ABC9] text-white flex items-center rounded-r-lg px-3">
+                      <MdPercent />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
+
           {/* SAVE PHRASE BUTTON----------- */}
           <button className="font-medium border rounded-lg border-black flex justify-center items-center gap-2 w-full text-lg md:h-12">
             <span>Guardar Cambios</span>
