e06cc7c8a9070b33f81538025e15daf810e7e83e
José Campos <josecamposh95@gmail.com>
2024-02-07 17:58:33 -0300

    feat: clear storage when catch 401 or 403 errors

--- src/services/api.services.js
+++ src/services/api.services.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { isAxiosError } from 'axios';
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -6,5 +6,21 @@ const apiService = axios.create({
   baseURL: apiUrl || 'http://localhost:3001/api',
   withCredentials: true,
 });
+
+apiService.interceptors.response.use(
+  response => response,
+  function (error) {
+    if (isAxiosError(error)) {
+      if (
+        (error.response.status === 401 || error.response.status === 403) &&
+        !window.location.pathname.includes('auth')
+      ) {
+        window.localStorage.clear();
+        window.location.reload();
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default apiService;
