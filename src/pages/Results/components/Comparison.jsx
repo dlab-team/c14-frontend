c0563609f5c8358cb9bb01616765a79b31e9122f
José Campos <josecamposh95@gmail.com>
2024-02-09 12:36:47 -0300

    add correct values to comparison chart

--- src/pages/Results/components/Comparison.jsx
+++ src/pages/Results/components/Comparison.jsx
@@ -1,16 +1,22 @@
-import './Comparison.css';
-import { testComparison } from './testComparison';
 import { PiInfoBold } from 'react-icons/pi';
 import { Tooltip } from '@/components/Tooltip';
+import useFormStore from '@/store/useFormStore';
+import './Comparison.css';
 
 function Comparison() {
+  const politicalResult = useFormStore(s =>
+    s.politicalResult.map(e => ({
+      ...e,
+      percentage: Math.floor(e.survey_results[0].percentage * 100),
+    }))
+  );
   return (
     <div className="items-center justify-center h-full lg:w-[70%] w-[90%] mx-auto">
       <div className="flex items-center justify-center text-3xl font-bold text-purple-800 mt-10 mb-10">
         Yo en comparación con otros participantes
       </div>
       <div className="flex items-center justify-end font-bold">Mis respuestas</div>
-      <div className="invisible md:visible md:flex items-center justify-begin lg:translate-x-[-5%] lg:translate-y-[15px] lg:pl-1 pl-10">
+      <div className="invisible lg:visible lg:flex items-center justify-begin lg:translate-x-[-5%] lg:translate-y-[15px] lg:pl-1 pl-10">
         <Tooltip
           message="<b>Enunciado</b><br><br>Frase contruida sobre la base de algunos de los temas por los cuales se producen debates políticos o culturales que sitúan a cada grupo en una posición"
           bgColor="bg-teal-600"
@@ -20,15 +26,15 @@ function Comparison() {
         </Tooltip>
       </div>
       <div className="scatter-chart mt-10">
-        {testComparison.map((item, index) => (
-          <div className="item body" key={index}>
-            <div className="legend mr-3 text-xs font-bold bottom-4 ml-[-50px]">
-              <label className="hidden md:block" htmlFor={`phrase-${index}`}>
-                {item.phrase}
+        {politicalResult.map((item, index) => (
+          <div className="item body" key={item.id}>
+            <div className="legend mr-3 text-xs bottom-4 ml-[-50px]">
+              <label className="text-xs mb-4 hidden lg:block" htmlFor={`phrase-${index}`}>
+                {item.text}
               </label>
 
-              <label className="block md:hidden pt-2" htmlFor={`phrase-${index}`}>
-                {item.item}
+              <label className="block lg:hidden pt-2" htmlFor={`phrase-${index}`}>
+                {index + 1}
               </label>
             </div>
             <div className="data">
@@ -37,36 +43,36 @@ function Comparison() {
               <div className="percentage fifty"></div>
               <div className="percentage seventy-five"></div>
               <div className="percentage one-hundred"></div>
-              <div className={`marker dot blue`} style={{ left: `${item.value}%` }}></div>
+              <div className={`marker dot blue`} style={{ left: `${item.percentage}%` }}></div>
             </div>
             <div className="response ml-3 font-bold w-40 text-center translate-y-[-10px] text-sm">
-              <span id={`phrase-${index}`}>{item.answer}</span>
+              {item.value}
             </div>
           </div>
         ))}
-
-        <div className="invisible md:visible md:flex items-center justify-end pr-10">
-          <Tooltip
-            message="Eje que indica el <b>porcentaje de acuerdo</b>"
-            bgColor="bg-purple-600"
-            width="w-[150px]"
-          >
-            <PiInfoBold className="w-6 h-6" />
-          </Tooltip>
-        </div>
-
-        <div className="flex items-center mt-10 justify-between w-[75%] mx-auto translate-x-[-16%] xl:w-[82%] xl:mx-auto xl:pl-9 xl:translate-x-0 lg:mx-auto lg:w-[78%] lg:pr-3 lg:translate-x-0 md:mx-auto md:w-[77%] md:pr-4 md:translate-x-0 sm:mx-auto sm:w-[81%] sm:pr-10 sm:translate-x-0">
-          <div className="text-sm">0%</div>
-          <div className="text-sm">25%</div>
-          <div className="text-sm">50%</div>
-          <div className="text-sm">75%</div>
-          <div className="text-sm">100%</div>
-        </div>
       </div>
-      <div className="mt-11 md:hidden">
-        {testComparison.map((item, index) => (
+
+      <div className="invisible lg:visible lg:flex items-center justify-end pr-10">
+        <Tooltip
+          message="Eje que indica el <b>porcentaje de acuerdo</b>"
+          bgColor="bg-purple-600"
+          width="w-[150px]"
+        >
+          <PiInfoBold className="w-6 h-6" />
+        </Tooltip>
+      </div>
+
+      <div className="flex items-center mt-10 justify-between w-[75%] mx-auto translate-x-[-16%] xl:w-[82%] xl:mx-auto xl:pl-9 xl:translate-x-0 lg:mx-auto lg:w-[78%] lg:pr-3 lg:translate-x-0 md:mx-auto md:w-[77%] md:pr-4 md:translate-x-0 sm:mx-auto sm:w-[81%] sm:pr-10 sm:translate-x-0">
+        <div className="text-sm">0%</div>
+        <div className="text-sm">25%</div>
+        <div className="text-sm">50%</div>
+        <div className="text-sm">75%</div>
+        <div className="text-sm">100%</div>
+      </div>
+      <div className="mt-11 lg:hidden">
+        {politicalResult.map((item, index) => (
           <div className="flex items-center justify-begin mt-2 w-[90%]" key={index}>
-            <div className="text-xs font-semibold">{item.item + '. ' + item.phrase}</div>
+            <div className="text-xs font-semibold">{index + 1 + '. ' + item.text}</div>
           </div>
         ))}
       </div>
