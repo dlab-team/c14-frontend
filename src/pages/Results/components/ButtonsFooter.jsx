3b4aea3758249dcd7d3da081dd9b78c11dfe3ab7
marcelobovet <marceloantoniobovet@gmail.com>
2024-02-21 14:28:48 -0300

    fix: position button footer in mobile, delete user

--- src/pages/Results/components/ButtonsFooter.jsx
+++ src/pages/Results/components/ButtonsFooter.jsx
@@ -1,11 +1,11 @@
-import instagramLogo from '../../../assets/img/instagramLogo.png';
 import facebookLogo from '../../../assets/img/facebookLogo.png';
+import instagramLogo from '../../../assets/img/instagramLogo.png';
 
 const hover = 'hover:bg-white hover:text-black hover:border-black hover:bg-opacity-10';
 
 export const ButtonsFooter = ({ title, link, instagram, facebook }) => {
   return (
-    <div className="flex flex-col justify-center text-white  lg:flex-row lg:justify-center lg:items-center lg:gap-[16px]">
+    <div className="flex flex-col justify-center text-white  lg:flex-row lg:justify-center lg:items-center lg:gap-[16px] text-center">
       <a href={link} target="_blank">
         <button
           className={`mb-[16px] px-[20px] py-[12px] rounded-md border-solid border-[1px] border-white font-semibold text-xl lg:mb-0 ${hover}`}
