d44cd421484483e4eda3d5e2ce43ed96813662cb
marcelobovet <marceloantoniobovet@gmail.com>
2024-02-22 03:32:34 -0300

    feat: footer & logo application in recognition view

--- src/components/Acknowledgments/Acknowledgments.jsx
+++ src/components/Acknowledgments/Acknowledgments.jsx
@@ -1,16 +1,23 @@
 import { Information } from './components/Information';
 import LeftSide from './components/LeftSide';
+import { ResultsFooter } from '@/pages/Results/components/ResultsFooter';
 import RightSide from './components/RightSide';
+import { useState } from 'react';
 
 const Acknowledgments = () => {
+  const [showImages, setShowImages] = useState(false);
+
   return (
-    <div className="flex flex-col justify-center items-center gap-10 px-8 mt-9">
-      <Information />
-      <div className="flex flex-col lg:flex-row items-center justify-center gap-y-12 gap-x-32 mb-12">
-        <LeftSide />
-        <RightSide />
+    <>
+      <div className="flex flex-col justify-center items-center gap-10 px-8 mt-9">
+        <Information />
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-y-12 gap-x-32 mb-12">
+          <LeftSide />
+          <RightSide />
+        </div>
       </div>
-    </div>
+      <ResultsFooter showImages={showImages} />
+    </>
   );
 };
 
--- src/components/Acknowledgments/components/RightSide.jsx
+++ src/components/Acknowledgments/components/RightSide.jsx
@@ -3,22 +3,33 @@ const RightSide = () => {
     'bg-gradient-to-r from-red-500 via-purple-600 via-blue-600 to-green-500 text-transparent bg-clip-text';
 
   return (
-    <div className="flex justify-center flex-col sm:flex-row gap-y-2 gap-x-5">
-      <div className="text-3xl font-bold flex flex-col gap-2 justify-between items-end">
-        <p>
-          Inspirar<span className={RainbowGradient}>NOS</span>
-        </p>
-        <p>
-          Incluir<span className={RainbowGradient}>NOS</span>
-        </p>
-        <p>
-          Innovar<span className={RainbowGradient}>NOS</span>
-        </p>
+    <>
+      <div className="flex flex-col items-center justify-center gap-5">
+        <div className="flex justify-center flex-col sm:flex-row gap-y-2 gap-x-5">
+          <div className="text-3xl font-bold flex flex-col gap-2 justify-between items-end">
+            <p>
+              Inspirar<span className={RainbowGradient}>NOS</span>
+            </p>
+            <p>
+              Incluir<span className={RainbowGradient}>NOS</span>
+            </p>
+            <p>
+              Innovar<span className={RainbowGradient}>NOS</span>
+            </p>
+          </div>
+          <div className="bg-slate-950 h-[11rem] w-[11rem] flex items-center justify-center">
+            <img src="/logo/logo-3xi-blanco.png" alt="Logo 3xi" className="h-[7.5rem]" />
+          </div>
+        </div>
+        <div className="bg-white  flex items-center justify-center">
+          <img
+            src="logo/criteria/criteriaLogo.png"
+            alt="Logo criteria"
+            className="w-[280px] h-[70px]"
+          />
+        </div>
       </div>
-      <div className="bg-slate-950 h-[11rem] w-[11rem] flex items-center justify-center">
-        <img src="/logo/logo-3xi-blanco.png" alt="Logo 3xi" className="h-[7.5rem]" />
-      </div>
-    </div>
+    </>
   );
 };
 
