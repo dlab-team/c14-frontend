5349ba17fb67399712e74d5723636fee9be8467f
marcelobovet <marceloantoniobovet@gmail.com>
2024-02-23 13:03:42 -0300

    fix: add footer & logo criteria in recognition view

--- src/components/Acknowledgments/Acknowledgments.jsx
+++ src/components/Acknowledgments/Acknowledgments.jsx
@@ -1,13 +1,16 @@
+import { useEffect, useState } from 'react';
+
 import { Information } from './components/Information';
 import LeftSide from './components/LeftSide';
+import { ResultsFooter } from '@/pages/Results/components/ResultsFooter';
 import RightSide from './components/RightSide';
-import { useNavigate } from 'react-router-dom';
 import useFormStore from '@/store/useFormStore';
-import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Acknowledgments = () => {
   const navigate = useNavigate();
   const totalPerceptionGap = useFormStore(state => state.totalPerceptionGap);
+  const [showImages, setShowImages] = useState(false);
 
   useEffect(() => {
     if (totalPerceptionGap === 0) {
@@ -16,13 +19,16 @@ const Acknowledgments = () => {
   }, [totalPerceptionGap, navigate]);
 
   return (
-    <div className="flex flex-col justify-center items-center gap-10 px-8 mt-9">
-      <Information />
-      <div className="flex flex-col lg:flex-row items-center justify-center gap-y-12 gap-x-32 mb-12">
-        <LeftSide />
-        <RightSide />
+    <>
+      <div className="flex flex-col justify-center items-center gap-10 px-8 mt-9">
+        <Information />
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-y-12 gap-x-32 mb-12">
+          <LeftSide />
+          <RightSide />
+        </div>
       </div>
-    </div>
+      <ResultsFooter showImages={showImages} />
+    </>
   );
 };
 
